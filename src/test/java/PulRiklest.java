a14ebab5676d52f614a15b25224bac26175ef40a
giciarz412 <tomaszfefler1990@gmail.com>
2018-02-24 13:55:02 +0100

    zadanie

--- src/test/java/PulRiklest.java
+++ src/test/java/PulRiklest.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class PulRiklest {
 
     public static void main(String[] args) {
 
-        System.out.println("Pul Rikłest 20:07");
+        /*
+        Na zadanie dostałem napisać funkcję, która przyjmuje
+        sekundy jako parametr i wypisuje na ekran czas w formacie hh:mm:ss.
+        1000 = 00:16:40
+        5000 = 01:23:20
+
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+
+        int hours = seconds / 3600;
+        int minutes = (seconds - (hours * 3600))/60;
+        int second = seconds - (hours*3600 + minutes*60);
+
+
+        if (hours < 10){
+            System.out.println("0"+hours+":"+minutes+":"+second);
+        }
+        else {
+            System.out.println(hours+":"+minutes+":"+second);
+        }
+
+
+
     }
 }
